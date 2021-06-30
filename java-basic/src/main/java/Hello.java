50113cef7cc2d93a2578e794775bc637c11a0695
juricho-jay <juri.cho.jay@gmail.com>
2021-06-30 17:39:45 +0900

    .

---
+++ java-basic/src/main/java/Hello.java
@@ -0,0 +1,11 @@
+class Hello {
+    //   ________________________________________
+    //   main()                                  |
+    //      - 프로그램 실행 시작점               |
+    //      - 'entry point'라 부름               |   
+    //   '//' 주석: 프로그램에 영향을 주지 않음  |
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("안녕하세요!");
+    }
+} 

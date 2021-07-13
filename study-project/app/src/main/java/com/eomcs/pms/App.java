dc9bb3ce627d3afcacf3ad14558a7e61756e0f61
juricho-jay <juri.cho.jay@gmail.com>
2021-07-13 20:59:43 +0900

    .

--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -15,24 +15,25 @@ import java.util.Scanner;
 //10) /project/list 명령 처리
 //11) /task/add 명령 처리
 //12) /task/list 명령 처리
+
 public class App {
 
   public static void main(String[] args) {
     Scanner keyboardScan = new Scanner(System.in);
 
-    // 회원 정보
+    //회원 정보
     final int LENGTH = 100;
     int[] no = new int[LENGTH];
     String[] name = new String[LENGTH];
     String[] email = new String[LENGTH];
     String[] password = new String[LENGTH];
-    String[] photo = new String[LENGTH];
+    String[] photo = new String[LENGTH]; 
     String[] tel = new String[LENGTH];
     Date[] registeredDate = new Date[LENGTH];
     int size = 0;
 
-    // 프로젝트 정보
-    final int PROJECT_LENGTH = 1000;
+    //프로젝트 정보
+    final int PROJECT_LENGTH = 100;
     int[] pNo = new int[PROJECT_LENGTH];
     String[] pTitle = new String[PROJECT_LENGTH];
     String[] pContent = new String[PROJECT_LENGTH];
@@ -42,7 +43,7 @@ public class App {
     String[] pMembers = new String[PROJECT_LENGTH];
     int pSize = 0;
 
-    // 작업 정보
+    //작업 정보
     final int TASK_LENGTH = 100;
     int[] tNo = new int[TASK_LENGTH];
     String[] tContent = new String[TASK_LENGTH];
@@ -50,6 +51,7 @@ public class App {
     String[] tOwner = new String[TASK_LENGTH];
     int[] tStatus = new int[TASK_LENGTH];
     int tSize = 0;
+
 
     while (true) {
       System.out.print("명령> ");
@@ -61,37 +63,31 @@ public class App {
         System.out.println("[회원 등록]");
         System.out.print("번호? ");
         no[size] = Integer.parseInt(keyboardScan.nextLine());
-
         System.out.print("이름? ");
         name[size] = keyboardScan.nextLine();
-
         System.out.print("이메일? ");
         email[size] = keyboardScan.nextLine();
-
         System.out.print("암호? ");
         password[size] = keyboardScan.nextLine();
-
         System.out.print("사진? ");
         photo[size] = keyboardScan.nextLine();
-
         System.out.print("전화? ");
         tel[size] = keyboardScan.nextLine();
-
         registeredDate[size] = new Date(System.currentTimeMillis());
-
         size++;
 
       } else if (input.equals("/member/list")) {
         System.out.println("[회원 목록]");
         for (int i = 0; i < size; i++) {
-          // 번호, 이름, 이메일, 전화, 가입일
-          System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
-              no[i], name[i], email[i], tel[i], registeredDate[i]);
+          System.out.printf("%d, %s, %s, %s, %tY-%5$tm-%5$td\n", 
+              no[i],
+              name[i],
+              email[i],
+              tel[i],
+              registeredDate[i]);
         }
-
-      }  else if (input.equals("/project/add")) {
+      } else if (input.equals("/project/add")) {
         System.out.println("[프로젝트 등록]");
-
         System.out.print("번호? ");
         pNo[pSize] = Integer.valueOf(keyboardScan.nextLine());
 
@@ -114,18 +110,14 @@ public class App {
         pMembers[pSize] = keyboardScan.nextLine();
 
         pSize++;
-
-      }  else if (input.equals("/project/list")) {
-        System.out.println("[프로젝트 목록]");
+      } else if (input.equals("/project/list")) {
         for (int i = 0; i < pSize; i++) {
           // 번호, 프로젝트명, 시작일, 종료일, 만든이
           System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
               pNo[i], pTitle[i], pStartDate[i], pEndDate[i], pOwner[i]);
         }
 
-      }  else if (input.equals("/task/add")) {
-        System.out.println("[작업 등록]");
-
+      } else if (input.equals("/task/add")) {
         System.out.print("번호? ");
         tNo[tSize] = Integer.parseInt(keyboardScan.nextLine());
 
@@ -147,9 +139,7 @@ public class App {
 
         tSize++;
 
-      }  else if (input.equals("/task/list")) {
-        System.out.println("[작업 목록]");
-
+      } else if (input.equals("/task/list")) {
         for (int i = 0; i < tSize; i++) {
           String stateLabel = null;
           switch (tStatus[i]) {
@@ -168,11 +158,12 @@ public class App {
         }
 
       } else {
-        System.out.println("실행할 수 없는 명령입니다.");
+        System.out.println("실행할 수 없는 명령어입니다.");
       }
-      System.out.println();
+
     }
 
-    keyboardScan.close();
+    keyboardScan.close(); // 데이터 입출력이 끝났으면 도구를 닫는다.
+
   }
 }

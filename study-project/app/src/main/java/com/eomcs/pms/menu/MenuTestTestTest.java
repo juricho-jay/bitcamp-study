1dde65a5d127b47ce0bbfe42f5a7c3cbf51178d7
juricho-jay <juri.cho.jay@gmail.com>
2021-08-04 02:54:37 +0900

    >\

---
+++ study-project/app/src/main/java/com/eomcs/pms/menu/MenuTestTestTest.java
@@ -0,0 +1,17 @@
+package com.eomcs.pms.menu;
+
+public class MenuTestTestTest {
+
+
+  // public abstract class Menu
+
+  String title;
+
+
+
+  // public Menu(String title) {
+  //   this.title = title;
+
+
+
+}

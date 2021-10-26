727d64ca114a2e4d3f65d9d9631e9df46ffd062a
juricho-jay <juri.cho.jay@gmail.com>
2021-10-26 14:21:45 +0900

    .

---
+++ study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberDetailHandler.java
@@ -0,0 +1,92 @@
+package com.eomcs.pms.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import com.eomcs.pms.dao.MemberDao;
+import com.eomcs.pms.domain.Member;
+
+@WebServlet("/member/detail")
+public class MemberDetailHandler extends GenericServlet {
+  private static final long serialVersionUID = 1L;
+
+  MemberDao memberDao;
+  SqlSession sqlSession;
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    try {
+      // Mybatis의 sqlSession 객체 준비
+      sqlSession = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(
+          "com/eomcs/pms/conf/mybatis-config.xml")).openSession();
+
+      // SqlSession 객체를 통해 MemberDao 구현체를 자동 생성한다.
+      memberDao = sqlSession.getMapper(MemberDao.class);
+
+    } catch (Exception e) {
+      throw new ServletException(e);
+    }
+  }
+
+
+  @Override
+  public void service(ServletRequest req, ServletResponse res)
+      throws ServletException, IOException {
+
+    res.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = res.getWriter();
+
+    out.println("<html>");
+    out.println("<head>");
+    out.println("   <title>회원상세</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>[회원 상세]</h1>");
+
+    int no = Integer.parseInt(req.getParameter("no"));
+
+    try {
+
+      Member member = memberDao.findByNo(no);
+      if (member == null) {
+        out.println("해당 번호의 회원이 없습니다.");
+        return;
+
+      } else {
+
+        out.printf("이름: %s<br>", member.getName());
+        out.printf("이메일: %s<br>", member.getEmail());
+        out.printf("사진: %s<br>", member.getPhoto());
+        out.printf("전화: %s<br>", member.getTel());
+        out.printf("등록일: %s<br>", member.getRegisteredDate());
+        out.println();
+
+        out.println("변경(U), 삭제(D), 이전(0) <br>");
+      }
+    } catch (Exception e) {
+      throw new ServletException(e);
+    }
+  }
+
+
+
+  @Override
+  public void destroy() {
+    sqlSession.close();    
+  }
+}
+
+
+
+
+
+
+
---
+++ study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberListHandler.java
@@ -0,0 +1,102 @@
+package com.eomcs.pms.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import com.eomcs.pms.dao.MemberDao;
+import com.eomcs.pms.domain.Member;
+
+
+@WebServlet("/member/list")
+public class MemberListHandler implements Servlet {
+
+  MemberDao memberDao;
+  SqlSession sqlSession;
+
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    try {
+      // Mybatis의 sqlSession 객체 준비
+      sqlSession = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(
+          "com/eomcs/pms/conf/mybatis-config.xml")).openSession();
+
+      // SqlSession 객체를 통해 MemberDao 구현체를 자동 생성한다.
+      memberDao = sqlSession.getMapper(MemberDao.class);
+
+    } catch (Exception e) {
+      throw new ServletException(e);
+    }
+  }
+
+  @Override
+  public void service(ServletRequest req, ServletResponse res) 
+      throws ServletException, IOException {
+
+    res.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = res.getWriter();
+
+    out.println("<html>");
+    out.println("<head>");
+    out.println("   <title>회원목록</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>[회원 목록]</h1>");
+
+    try {
+
+      Collection<Member> memberList = memberDao.findAll();
+
+      for (Member member : memberList) {
+        out.printf("%d, <a href='detail?no=%1$d'>%s</a>, %s, %s, %s<br>", 
+            member.getNo(), 
+            member.getName(), 
+            member.getEmail(), 
+            member.getTel(), 
+            member.getRegisteredDate());
+      }
+    } catch (Exception e) {
+      throw new ServletException(e);
+    }
+    out.println("</body>");
+    out.println("</html>");
+  }
+
+
+  @Override
+  public ServletConfig getServletConfig() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+
+
+  @Override
+  public String getServletInfo() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+
+
+  @Override
+  public void destroy() {
+    sqlSession.close();
+  }
+}
+
+
+
+
+
+
+
